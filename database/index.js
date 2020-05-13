4876e1bce277383b346155c7df936153f6daea66
Filip Leitner <leitnerfilip@gmail.com>
2020-05-13 15:30:21 +0200

    database connection

--- database/index.js
+++ database/index.js
@@ -3,9 +3,8 @@ const pgp = require("pg-promise")({});
 const cn = {
   host: "10.0.0.26",
   port: 5432,
-  database: "dp-app",
-  user: "postgres",
-  password: "postgres"
+  database: "fleitner",
+  user: "fleitner",
 };
 
 const db = pgp(cn);
